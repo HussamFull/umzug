c46613624de6e601d1f820f91e050a59dfe9d019
hamada <bhak6150@gmail.com>
2025-09-24 22:14:34 +0200

    Fix Email

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -70,10 +70,10 @@ export default function Contact() {
               <div className="row">
                 <div className="col-md-6 form-group">
                   {/* تأكد من أن كل input له name مطابق للمتغير في قالب EmailJS */}
-                  <input type="text" name="user_name" className="form-control" id="name" placeholder="Your Name" required />
+                  <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" required />
                 </div>
                 <div className="col-md-6 form-group mt-3 mt-md-0">
-                  <input type="email" className="form-control" name="user_email" id="email" placeholder="Your Email" required />
+                  <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" required />
                 </div>
               </div>
               <div className="form-group mt-3">
