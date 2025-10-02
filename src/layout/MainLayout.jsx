7d5350a5060277f1568dafbe6bb6d7f93fdf9bab
hamada <bhak6150@gmail.com>
2025-10-02 22:23:25 +0200

    fix

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import Navbar from '../components/Navbar/Navbar.jsx'
-import Footer from '../components/footer/Footer'
+import Navbar from '../components/navbar/Navbar.jsx'
+
+import Footer from '../components/footer/Footer.jsx'
 import { Outlet } from 'react-router-dom'
 
 export default function MainLayout() {
